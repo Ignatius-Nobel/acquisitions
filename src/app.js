9998dda3b3e48429a93fc89040477d0e927ec6eb
Ignatius <csnobel2001@gmail.com>
2025-12-14 10:35:05 +0530

    Create auth routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import authRoutes from '#routes/auth.routes.js';
 
 const app = express();
 
@@ -18,5 +19,15 @@ app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
   res.status(200).send('Hello from Acquisitions API!');
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).send({status: 'OK', timestamp: new Date().toISOString(), uptime: process.uptime()});
+});
+
+app.get('/api', (req,res) => {
+  res.status(200).json('Acquisitions API is running');
+});
+
+app.use('/api/auth', authRoutes);
 
 export default app;
